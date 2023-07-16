2ed5e5b4bb420c1120d6188c250ccde5954c1591
dnamani1 <139375828+dnamani1@users.noreply.github.com>
2023-07-16 16:34:31 -0500

    Update resistor-calculator.component.spec.ts
    fully unit test the service

--- src/app/resistor-calculator/resistor-calculator.component.spec.ts
+++ src/app/resistor-calculator/resistor-calculator.component.spec.ts
@@ -1,17 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { ResistorCalculatorComponent } from './resistor-calculator.component';
 
 describe('ResistorCalculatorComponent', () => {
   let component: ResistorCalculatorComponent;
   let fixture: ComponentFixture<ResistorCalculatorComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ResistorCalculatorComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ResistorCalculatorComponent],
+      imports: [FormsModule]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ResistorCalculatorComponent);
@@ -19,7 +19,28 @@ describe('ResistorCalculatorComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should calculate resistance correctly', () => {
+    component.selectedColors = ['Brown', 'Red', 'Orange', '100', '±2%'];
+    const resistance = component.calculateResistance();
+    expect(resistance).toBe('12300000.00 ±2%');
+  });
+
+  it('should get resistance value correctly', () => {
+    const resistanceValue = component.getResistanceValue('Brown', 'Red', 'Orange');
+    expect(resistanceValue).toBe(123000);
+  });
+
+  it('should get multiplier label correctly', () => {
+    const multiplierLabel = component.getMultiplierLabel('10K');
+    expect(multiplierLabel).toBe('Yellow');
+  });
+
+  it('should get tolerance label correctly', () => {
+    const toleranceLabel = component.getToleranceLabel('±5%');
+    expect(toleranceLabel).toBe('Gold');
   });
 });
