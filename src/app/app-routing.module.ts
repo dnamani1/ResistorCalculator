7c46b845d1d85d13d7b7260220adab761873d972
dnamani1 <139375828+dnamani1@users.noreply.github.com>
2023-07-16 16:13:01 -0500

    Update app-routing.module.ts
    Updated the changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ResistorCalculatorComponent } from './resistor-calculator/resistor-calculator.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'calculator', pathMatch: 'full' },
+  { path: 'calculator', component: ResistorCalculatorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
